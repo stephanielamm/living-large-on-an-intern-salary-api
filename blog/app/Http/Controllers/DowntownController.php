9bc8c4795b9dafd4a248ad3ecf3e131acb5f608c
stephanielamm <slamm5@live.unc.edu>
2017-04-05 21:19:36 -0400

    Created Downtown and Midtown Controllers.

---
+++ blog/app/Http/Controllers/DowntownController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Downtown;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+use App\Http\Requests;
+
+class DowntownController extends Controller
+{
+
+  }
+}
---
+++ blog/app/Http/Controllers/MidtownController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Midtown;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+
+use App\Http\Requests;
+
+class MidtownController extends Controller
+{
+
+  }
+}
